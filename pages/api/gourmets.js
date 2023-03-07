f8743daa957133601399de32ff09182993d4235e
1nary <naritoshi.2714.p3@gmail.com>
2023-03-07 15:50:40 +0900

    値を変数に入れた

--- pages/api/gourmets.js
+++ pages/api/gourmets.js
@@ -1,13 +1,14 @@
 import getConfig from 'next/config';
+import { LAT_CAMPUS, LNG_CAMPUS, SHOP_COUNT } from '/config/constants'
 
 const shops = async (req, res) => {
   const { HOTPEPPER_API_KEY } = getConfig().serverRuntimeConfig;
 
   const query = new URLSearchParams();
   query.set('key', HOTPEPPER_API_KEY);
-  query.set('lat', '26.26304196188529');
-  query.set('lng', '127.75327304084749');
-  query.set('count', '100');
+  query.set('lat', LAT_CAMPUS);
+  query.set('lng', LNG_CAMPUS);
+  query.set('count', SHOP_COUNT);
   query.set('range', '5');
   query.set('format', 'json');
 
